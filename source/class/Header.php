208b497f39b00be0515d1e2d966d4b4bd95e0b12
Julien Delsescaux <julien.delsescaux@habitatpresto.com>
2017-06-09 09:03:15 +0200

    synchro

--- source/class/Header.php
+++ source/class/Header.php
@@ -4,27 +4,35 @@
 namespace Phi\HTTP;
 
 
-
-
 class Header
 {
 
-	protected $name;
-	protected $value;
+    protected $name;
+    protected $value;
 
 
-	public function __construct($name, $value) {
-		$this->name=$name;
-		$this->value=$value;
-	}
+    public function __construct($name, $value)
+    {
+        $this->name = $name;
+        $this->value = $value;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
 
 
-	public function send() {
-		header($this->name.': '.$this->value);
-		return $this;
-	}
-
-
+    public function send()
+    {
+        header($this->name . ': ' . $this->value);
+        return $this;
+    }
 
 
 }
--- source/class/Response.php
+++ source/class/Response.php
@@ -17,5 +17,29 @@ class Response
         $this->headers = $headers;
     }
 
+    public function addHeader($nameOrHeader = null, $value = null)
+    {
+        if ($nameOrHeader instanceof Header) {
+            $name = $nameOrHeader->getName();
+            $value = $nameOrHeader->getValue();
+        } else {
+            $name = $nameOrHeader;
+        }
+
+        $header = new Header($name, $value);
+        $this->headers[$name] = $header;
+        return $this;
+    }
+
+
+    public function send()
+    {
+
+        foreach ($this->headers as $header) {
+            $header->send();
+        }
+        return $this->content;
+
+    }
 
 }
