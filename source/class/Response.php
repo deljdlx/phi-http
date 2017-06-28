68f90614b01fb78c7dddac0f25a6e8a067ef244f
Julien Delsescaux <julien.delsescaux@habitatpresto.com>
2017-06-28 16:52:43 +0200

    synchro

--- source/class/Response.php
+++ source/class/Response.php
@@ -15,6 +15,11 @@ class Response extends Message
     protected $content = '';
 
 
+    /**
+     * Response constructor.
+     * @param string $content
+     * @param Header[] $headers
+     */
     public function __construct($content = '', array $headers = array())
     {
         $this->content = $content;
