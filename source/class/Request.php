31329235afcf6ecdb3690b5e02ce8c3aa042cd51
Jean luc <you@example.com>
2018-01-17 10:35:29 +0100

    synchro

--- source/class/Request.php
+++ source/class/Request.php
@@ -7,6 +7,12 @@ namespace Phi\HTTP;
 class Request extends Message
 {
 
+    const KEY_URI = 'REQUEST_URI';
+    const KEY_PROTOCOL = 'REQUEST_PROTOCOL';
+    const KEY_SERVER_NAME = 'SERVER_NAME';
+    const KEY_REQUEST_METHOD = 'REQUEST_METHOD';
+    const KEY_GET_ALL_HEADERS = 'getallheaders';
+
 
     protected $uri = null;
     protected $verb = null;
@@ -29,24 +35,24 @@ class Request extends Message
 
         $this->serverVariables = $_SERVER;
 
-        if (array_key_exists('REQUEST_URI', $_SERVER)) {
-          $this->uri = $_SERVER['REQUEST_URI'];
+        if (array_key_exists(static::KEY_URI, $_SERVER)) {
+          $this->uri = $_SERVER[static::KEY_URI];
         }
 
-        if (array_key_exists('SERVER_PROTOCOL', $_SERVER)) {
-          $this->protocol = $_SERVER['SERVER_PROTOCOL'];
+        if (array_key_exists(static::KEY_PROTOCOL, $_SERVER)) {
+          $this->protocol = $_SERVER[static::KEY_PROTOCOL];
           $this->normalizedProtocol = strtolower( preg_replace('`^(\w+).*`', '$1', $this->protocol));
         }
 
-        if (array_key_exists('SERVER_NAME', $_SERVER)) {
-          $this->hostname = $_SERVER['SERVER_NAME'];
+        if (array_key_exists(static::KEY_SERVER_NAME, $_SERVER)) {
+          $this->hostname = static::KEY_SERVER_NAME;
         }
 
-        if (array_key_exists('REQUEST_METHOD', $_SERVER)) {
-          $this->verb = $_SERVER['REQUEST_METHOD'];
+        if (array_key_exists(static::KEY_REQUEST_METHOD, $_SERVER)) {
+          $this->verb = $_SERVER[static::KEY_REQUEST_METHOD];
         }
 
-        if(function_exists('getallheaders')) {
+        if(function_exists(static::KEY_GET_ALL_HEADERS)) {
             $headers = getallheaders();
             foreach ($headers as $name => $value) {
               $header = new Header($name, $value);
@@ -74,7 +80,8 @@ class Request extends Message
     }
 
 
-    public function getVerb() {
+    public function getVerb()
+    {
         return $this->verb;
     }
 
